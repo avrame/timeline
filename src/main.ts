d327553506cb931326546f5f4059e7fa6e4eab90
Avram Eisner <avrame@gmail.com>
2023-11-22 16:22:05 -0800

    - made timeline take up half the page - prevent overscroll on page - added todo.md - added constants for specifying when ticks and labels are visible - labels go in their own container - added BCE/CE to years

--- src/main.ts
+++ src/main.ts
@@ -14,60 +14,87 @@ const app = new PIXI.Application({
 })
 
 const VIEW_X_POS = timeline_div?.getBoundingClientRect().x ?? 0
-const VIEW_X_MARGIN = 20
+const VIEW_X_MARGIN = 35
+
+const now = new Date()
 const START_YEAR = 0
-const END_YEAR = 2000
+const END_YEAR = now.getFullYear()
 const YEAR_SPAN = END_YEAR - START_YEAR
+
 const MIN_ZOOM = 1
-const MAX_ZOOM = 100
+const MAX_ZOOM = YEAR_SPAN / 2
 const ZOOM_RATE = .005
+
 const YEAR_TICK_HEIGHT = 32
 const DECADE_TICK_HEIGHT = 64
 const CENTURY_TICK_HEIGHT = 96
+
+const YEAR_SPAN_CENTURY_TICK_VISIBLE = 2500
+const YEAR_SPAN_CENTURY_LABEL_VISIBLE = 2500
+const YEAR_SPAN_DECADE_TICK_VISIBLE = 1000
+const YEAR_SPAN_DECADE_LABEL_VISIBLE = 275
+const YEAR_SPAN_YEAR_TICK_VISIBLE = 250
+const YEAR_SPAN_YEAR_LABEL_VISIBLE = 35
 
 let start_year = START_YEAR
 let year_span = YEAR_SPAN
 let pixels_per_year = (app.view.width - 2 * VIEW_X_MARGIN) / year_span
 let zoom = 1.0
-let global_mouse_x = 0
+let global_mouse_x = app.view.width / 2
 let mouse_x = 0
-let mouse_year = START_YEAR + YEAR_SPAN / 2
+let mouse_year
 
 timeline_div?.appendChild(app.view as unknown as Node)
 
 const timeline_container = new PIXI.Container()
-const label_container = new PIXI.Container()
+const century_label_container = new PIXI.Container()
+const decade_label_container = new PIXI.Container()
+const year_label_container = new PIXI.Container()
 
 const century_ticks = new PIXI.Graphics()
 const decade_ticks = new PIXI.Graphics()
 const year_ticks = new PIXI.Graphics()
 
 const century_text_style = new PIXI.TextStyle({
-  fontSize: 16,
+  fontSize: 14,
 })
 const century_labels: { [year: number]: PIXI.Text } = {}
 for (let y = START_YEAR; y <= END_YEAR; y += 100) {
-  const label = new PIXI.Text(y.toString(), century_text_style)
+  const label = new PIXI.Text(format_year(y), century_text_style)
   label.x = (y - START_YEAR) * pixels_per_year + VIEW_X_MARGIN
   label.y = app.view.height - CENTURY_TICK_HEIGHT
   label.anchor.set(0.5, 1)
-  label_container.addChild(label)
+  century_label_container.addChild(label)
   century_labels[y] = label
 }
 
 const decade_text_style = new PIXI.TextStyle({
-  fontSize: 14,
+  fontSize: 12,
 })
 const decade_labels: { [year: number]: PIXI.Text } = {}
 for (let y = START_YEAR; y <= END_YEAR; y += 10) {
   if (y % 100 !== 0) {
-    const label = new PIXI.Text(y.toString(), decade_text_style)
+    const label = new PIXI.Text(format_year(y), decade_text_style)
     label.x = (y - START_YEAR) * pixels_per_year + VIEW_X_MARGIN
     label.y = app.view.height - DECADE_TICK_HEIGHT
     label.anchor.set(0.5, 1)
-    label.visible = false
-    label_container.addChild(label)
+    decade_label_container.addChild(label)
     decade_labels[y] = label
+  }
+}
+
+const year_text_style = new PIXI.TextStyle({
+  fontSize: 10,
+})
+const year_labels: { [year: number]: PIXI.Text } = {}
+for (let y = START_YEAR; y <= END_YEAR; y += 1) {
+  if (y % 10 !== 0) {
+    const label = new PIXI.Text(format_year(y), year_text_style)
+    label.x = (y - START_YEAR) * pixels_per_year + VIEW_X_MARGIN
+    label.y = app.view.height - YEAR_TICK_HEIGHT
+    label.anchor.set(0.5, 1)
+    year_label_container.addChild(label)
+    year_labels[y] = label
   }
 }
 
@@ -75,7 +102,9 @@ timeline_container
   .addChild(century_ticks)
   .addChild(decade_ticks)
   .addChild(year_ticks)
-  .addChild(label_container)
+  .addChild(century_label_container)
+  .addChild(decade_label_container)
+  .addChild(year_label_container)
 app.stage.addChild(timeline_container)
 draw_ticks()
 
@@ -91,7 +120,7 @@ app.stage.addEventListener('wheel', handle_mouse_wheel)
 app.ticker.add(() => {
   mouse_x = global_mouse_x - VIEW_X_POS - VIEW_X_MARGIN
   mouse_year = start_year + mouse_x / pixels_per_year
-  if (mouse_year_b) mouse_year_b.innerText = Math.floor(mouse_year).toString()
+  if (mouse_year_b) mouse_year_b.innerText = format_year(mouse_year).toString()
   start_year = START_YEAR - timeline_container.x / pixels_per_year
   draw_ticks()
   update_label_positions()
@@ -118,13 +147,18 @@ function handle_mouse_wheel(e: WheelEvent) {
   timeline_container.x = x_offset
 }
 
+function format_year(year: number) {
+  const era = year < 0 ? 'BCE' : 'CE'
+  return `${Math.abs(Math.floor(year))} ${era}`
+}
+
 function calc_pixels_per_year(year_span: number) {
   return (app.view.width - 2 * VIEW_X_MARGIN) / year_span
 }
 
 function calc_zoom(delta_y: number): number {
   const zoom_delta = delta_y * ZOOM_RATE
-  const new_zoom = zoom + zoom_delta
+  const new_zoom = zoom * (1 + zoom_delta)
   let zoom_mult = 1
   if (new_zoom < MIN_ZOOM) {
     zoom = MIN_ZOOM
@@ -140,16 +174,16 @@ function calc_zoom(delta_y: number): number {
 
 function draw_ticks() {
   console.log(year_span)
-  if (year_span <= 200) { draw_year_ticks() } else { year_ticks.clear() }
-  if (year_span <= 340) { draw_decade_ticks() } else { decade_ticks.clear() }
-  draw_century_ticks()
+  if (year_span <= YEAR_SPAN_YEAR_TICK_VISIBLE) { draw_year_ticks() } else { year_ticks.clear() }
+  if (year_span <= YEAR_SPAN_DECADE_TICK_VISIBLE) { draw_decade_ticks() } else { decade_ticks.clear() }
+  if (year_span <= YEAR_SPAN_CENTURY_TICK_VISIBLE) { draw_century_ticks() } else { century_ticks.clear() }
 }
 
 function draw_year_ticks() {
   year_ticks.clear()
   year_ticks.lineStyle({ width: 1, color: theme['year-tick-color'] })
   const start = Math.floor(start_year - START_YEAR)
-  const end = Math.ceil(start + year_span)
+  const end = Math.ceil(start + year_span + 5)
   for (let year_count = start; year_count <= end; year_count += 1) {
     if (year_count % 10 !== 0) {
       const x_pos = year_count * pixels_per_year + VIEW_X_MARGIN
@@ -164,11 +198,13 @@ function draw_decade_ticks() {
   decade_ticks.lineStyle({ width: 2, color: theme['decade-tick-color'] })
   const start_decade = Math.floor(start_year / 10) * 10
   const start = Math.floor(start_decade - START_YEAR)
-  const end = Math.ceil(start_decade + year_span)
+  const end = Math.ceil(start_decade + year_span + 10)
   for (let year_count = start; year_count <= end; year_count += 10) {
-    const x_pos = year_count * pixels_per_year + VIEW_X_MARGIN
-    decade_ticks.moveTo(x_pos, app.view.height)
-    decade_ticks.lineTo(x_pos, app.view.height - DECADE_TICK_HEIGHT)
+    if (year_count % 100 !== 0) {
+      const x_pos = year_count * pixels_per_year + VIEW_X_MARGIN
+      decade_ticks.moveTo(x_pos, app.view.height)
+      decade_ticks.lineTo(x_pos, app.view.height - DECADE_TICK_HEIGHT)
+    }
   }
 }
 
@@ -177,7 +213,7 @@ function draw_century_ticks() {
   century_ticks.lineStyle({ width: 2, color: theme['century-tick-color'] })
   const start_century = Math.floor(start_year / 100) * 100
   const start = Math.floor(start_century - START_YEAR)
-  const end = Math.ceil(start_century + year_span)
+  const end = Math.ceil(start_century + year_span + 100)
   for (let year = start; year <= end; year += 100) {
     const x_pos = year * pixels_per_year + VIEW_X_MARGIN
     century_ticks.moveTo(x_pos, app.view.height)
@@ -186,25 +222,73 @@ function draw_century_ticks() {
 }
 
 function update_label_positions() {
-  update_decade_label_positions()
-  update_century_label_positions()
+  const years_in_margin = VIEW_X_MARGIN / pixels_per_year
+  const visible_start_year = start_year - years_in_margin
+  const visible_end_year = visible_start_year + year_span + 2 * years_in_margin
+
+  if (year_span <= YEAR_SPAN_YEAR_LABEL_VISIBLE) {
+    year_label_container.visible = true
+    update_year_label_positions(visible_start_year, visible_end_year)
+  } else {
+    year_label_container.visible = false
+  }
+
+  if (year_span <= YEAR_SPAN_DECADE_LABEL_VISIBLE) {
+    decade_label_container.visible = true
+    update_decade_label_positions(visible_start_year, visible_end_year)
+  } else {
+    decade_label_container.visible = false
+  }
+
+  if (year_span <= YEAR_SPAN_CENTURY_LABEL_VISIBLE) {
+    century_label_container.visible = true
+    update_century_label_positions(visible_start_year, visible_end_year)
+  } else {
+    century_label_container.visible = false
+  }
 }
 
-function update_decade_label_positions() {
-  for (let year = 0; year <= YEAR_SPAN; year += 10) {
-    const label = decade_labels[year + START_YEAR]
+function update_year_label_positions(visible_start_year: number, visible_end_year: number) {
+  for (let year_count = 0; year_count <= YEAR_SPAN; year_count += 1) {
+    const year = year_count + START_YEAR
+    const label = year_labels[year]
     if (label) {
-      label.x = year * pixels_per_year + VIEW_X_MARGIN
-      label.visible = year_span <= 340 ? true : false
+      if (year >= visible_start_year && year <= visible_end_year) {
+        label.x = year_count * pixels_per_year + VIEW_X_MARGIN
+        label.visible = true
+      } else {
+        label.visible = false
+      }
     }
   }
 }
 
-function update_century_label_positions() {
-  for (let year = 0; year <= YEAR_SPAN; year += 100) {
-    const label = century_labels[year + START_YEAR]
+function update_decade_label_positions(visible_start_year: number, visible_end_year: number) {
+  for (let year_count = 0; year_count <= YEAR_SPAN; year_count += 10) {
+    const year = year_count + START_YEAR
+    const label = decade_labels[year]
     if (label) {
-      label.x = year * pixels_per_year + VIEW_X_MARGIN
+      if (year >= visible_start_year && year <= visible_end_year) {
+        label.x = year_count * pixels_per_year + VIEW_X_MARGIN
+        label.visible = true
+      } else {
+        label.visible = false
+      }
+    }
+  }
+}
+
+function update_century_label_positions(visible_start_year: number, visible_end_year: number) {
+  for (let year_count = 0; year_count <= YEAR_SPAN; year_count += 100) {
+    const year = year_count + START_YEAR
+    const label = century_labels[year]
+    if (label) {
+      if (year >= visible_start_year && year <= visible_end_year) {
+        label.x = year_count * pixels_per_year + VIEW_X_MARGIN
+        label.visible = true
+      } else {
+        label.visible = false
+      }
     }
   }
 }
---
+++ todo.md
@@ -0,0 +1,7 @@
+# To Do
+
+## Bugs
+- when scrolled all the way to the end year, gets stuck - can't scroll left
+
+## Features
+- fade labels/ticks in and out
